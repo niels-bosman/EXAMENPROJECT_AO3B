135b10faa72a085208ccf40387dda98442f9a64b
Clemens van Marwijk <clemens.marwijk@gc.graafschapcollege.nl>
2019-11-14 14:47:11 +0100

    Re added previous commit

--- restaurant-de-graaf/resources/views/account_not_activated.blade.php
+++ restaurant-de-graaf/resources/views/account_not_activated.blade.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
     <head>
         <script
@@ -8,12 +7,12 @@
         </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Welkom bij de graaf</title>
+        <title>Uw account is nog niet geactiveerd</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <link rel="stylesheet" href="../sass/app.scss">
     </head>
     <body>
-        <h1>Welcome!</h1>
+        <h1>The account you have created has not yet been activated!</h1>
     </body>
 </html>
