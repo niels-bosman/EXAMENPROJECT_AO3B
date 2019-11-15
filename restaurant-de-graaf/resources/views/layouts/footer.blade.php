a5aae1cd27d3c13dad74e162d139b0b7ad7e246f
Niels Bosman <nielsbosman33@icloud.com>
2019-11-15 11:32:37 +0100

    Social media icons

--- restaurant-de-graaf/resources/views/layouts/footer.blade.php
+++ restaurant-de-graaf/resources/views/layouts/footer.blade.php
@@ -2,11 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                <h2 class="footer__menu-title">Menu</h2>
+                <h2 class="footer__menu-title">Pagina's</h2>
                 <ul class="footer__menu">
                     <li ><a class="footer__menu-item" href="#">Reserveren</a></li>
                     <li><a class="footer__menu-item" href="#">Menu</a></li>
                     <li><a class="footer__menu-item"  href="#">Contact</a></li>
+                    <li><a class="footer__menu-item"  href="#">F.A.Q.</a></li>
+                    <li><a class="footer__menu-item"  href="#">Servicevoorwaarden</a></li>
                 </ul>
             </div>
             <div class="col-md-4">
@@ -15,6 +17,14 @@
                     <li ><a class="footer__menu-item" href="#">Inloggen</a></li>
                     <li><a class="footer__menu-item" href="#">Registreren</a></li>
                     <li><a class="footer__menu-item"  href="#">Wachtwoord vergeten</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h2 class="footer__menu-title">Social media</h2>
+                <ul class="footer__menu-socials">
+                    <a class="footer__menu-item" href="#"><i class="fa fa-facebook-square"></i></a>
+                    <a class="footer__menu-item" href="#"><i class="fa fa-twitter-square"></i></a>
+                    <a class="footer__menu-item" href="#"><i class="fa fa-instagram"></i></a>
                 </ul>
             </div>
         </div>
