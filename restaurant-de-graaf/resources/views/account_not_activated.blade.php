bd6fab122c4bc095047f4a0b6313c629429ee5b1
Clemens van Marwijk <clemens.marwijk@gc.graafschapcollege.nl>
2019-11-15 08:53:29 +0100

    Implemented working page parts

--- restaurant-de-graaf/resources/views/account_not_activated.blade.php
+++ restaurant-de-graaf/resources/views/account_not_activated.blade.php
@@ -1,18 +1,11 @@
-<html>
-    <head>
-        <script
-            src="https://code.jquery.com/jquery-3.4.1.min.js"
-            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous">
-        </script>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Uw account is nog niet geactiveerd</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <link rel="stylesheet" href="../sass/app.scss">
-    </head>
-    <body>
-        <h1>The account you have created has not yet been activated!</h1>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('sidebar')
+
+@endsection
+
+@section('content')
+<h1>The account you have created has not yet been activated!</h1>
+@endsection
--- restaurant-de-graaf/resources/views/welcome.blade.php
+++ restaurant-de-graaf/resources/views/welcome.blade.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <script
-            src="https://code.jquery.com/jquery-3.4.1.min.js"
-            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous">
-        </script>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Welkom bij de graaf</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <link rel="stylesheet" href="../sass/app.scss">
-    </head>
-    <body>
-        <h1>Welcome!</h1>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('sidebar')
+
+@endsection
+
+@section('content')
+    <h1>Hier komt de nieuwe applicatie</h1>
+@endsection
+
+@section('footer')
+
+@endsection
