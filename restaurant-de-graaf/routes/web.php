d2f5e7f0092d1ba1520bfa5d273f7d0569749055
Niels Bosman <nielsbosman33@icloud.com>
2019-11-15 13:36:30 +0100

    Account blocked page

--- restaurant-de-graaf/routes/web.php
+++ restaurant-de-graaf/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 Route::get('/', 'PagesController@home');
+
 Route::get('/account_not_activated', 'PagesController@account_not_activated');
+
+Route::get('/account_blocked', 'PagesController@account_blocked');
 
