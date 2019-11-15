64473ce2742abefc817004cd2d04f6f4aab8456c
Clemens van Marwijk <clemens.marwijk@gc.graafschapcollege.nl>
2019-11-15 10:53:10 +0100

    Created activation page

--- restaurant-de-graaf/resources/views/account_not_activated.blade.php
+++ restaurant-de-graaf/resources/views/account_not_activated.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 
-@section('title', 'Home')
+@section('title', 'Het account is nog niet geactiveerd')
 
-@section('sidebar')
+@section('header')
 
 @endsection
 
 @section('content')
-<h1>The account you have created has not yet been activated!</h1>
+    <h1>Uw account is nog niet geactiveerd!</h1>
+    <p>Zo te zien heeft u nog niet het account geactiveerd. <br>
+        Check je mail of u een bevestigingsmail heb gekregen en klik op de link. <br><br>
+        daarna wordt u account geactiveerd en kunt u inloggen. <br><br>
+        Hulp nodig of geen link gekregen? neem contact met ons op. <br>
+        (+316) 123 456
 @endsection
--- restaurant-de-graaf/routes/web.php
+++ restaurant-de-graaf/routes/web.php
@@ -1,20 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'PagesController@home');
+Route::get('/account_not_activated', 'PagesController@account_not_activated');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/account_not_activated', function () {
-    return view('account_not_activated');
-});
