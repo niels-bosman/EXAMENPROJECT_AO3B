34ecc807041fe72cfabdabc004305db755588262
Niels Bosman <nielsbosman33@icloud.com>
2019-11-18 09:44:57 +0100

    Login and logout works

--- restaurant-de-graaf/routes/web.php
+++ restaurant-de-graaf/routes/web.php
@@ -5,3 +5,9 @@ Route::get('/', 'PagesController@home');
 Route::get('/account_not_activated', 'PagesController@account_not_activated');
 
 Route::get('/account_blocked', 'PagesController@account_blocked');
+
+Route::get('logout', 'Auth\LoginController@logout', function () {
+    return abort(404);
+});
+
+Auth::routes();
