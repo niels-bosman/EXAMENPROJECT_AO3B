b87189dfd6fc585a8012a595fb628e0297f11f64
Niels Bosman <nielsbosman33@icloud.com>
2019-11-19 22:15:00 +0100

    Img and text improvements

--- restaurant-de-graaf/resources/views/home.blade.php
+++ restaurant-de-graaf/resources/views/home.blade.php
@@ -13,12 +13,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <h2>Onze gerechten</h2>
+                    <h2>Hoogwaardige ingredienten</h2>
                     <p>Onze maaltijden zijn gemaakt met de hoogste kwaliteit en zorg. Elk seizoen wordt het menu anders en kun je lekker afwisselen met de maaltijden, daarmee kun je de kinderen steeds opnieuw verrassen. </p>
                     <a class="menu__button button--primary" href="/menu">Bekijk ons menu</a>
                 </div>
                 <div class="col-md-6">
-                    <img class="menu__img" src="https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    <img class="menu__img" src="https://images.pexels.com/photos/1756061/pexels-photo-1756061.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
                 </div>
             </div>
         </div>
