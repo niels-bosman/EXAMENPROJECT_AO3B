3a5940c7c787e26c319a2fcd4e274e592f09e4c7
majd wenni <majd.wenni@student.graafschapcollege.nl>
2019-11-19 12:42:50 +0100

    no robot klaar

--- restaurant-de-graaf/app/Http/Controllers/Auth/RegisterController.php
+++ restaurant-de-graaf/app/Http/Controllers/Auth/RegisterController.php
@@ -48,12 +48,20 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        //dd($data);
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'tel_number' => ['required', 'string', 'max:255'],
-        ]);
+            'no_robot' => ['required']
+            //'no_robot' => ['required', 'boolean']
+
+        ],
+        [
+            'no_robot.required' => 'je moet het vakje eerst aanvinken voordat je verder kan'
+        ]
+        );
     }
 
     /**
--- restaurant-de-graaf/resources/views/auth/register.blade.php
+++ restaurant-de-graaf/resources/views/auth/register.blade.php
@@ -43,7 +43,7 @@
                             <label for="street" class="col-md-4 col-form-label text-md-right">{{ __('Straat') }}</label>
 
                             <div class="col-md-6">
-                                <input id="street" type="text" class="form-control @error('street') is-invalid @enderror" name="street" value="{{ old('street') }}" required autocomplete="street" autofocus>
+                                <input id="street" type="text" class="form-control @error('street') is-invalid @enderror" name="street" value="{{ old('street') }}">
 
                                 @error('street')
                                 <span class="invalid-feedback" role="alert">
@@ -57,7 +57,7 @@
                             <label for="house_number" class="col-md-4 col-form-label text-md-right">{{ __('Huisnummer') }}</label>
 
                             <div class="col-md-6">
-                                <input id="house_number" type="text" class="form-control @error('house_number') is-invalid @enderror" name="house_number" value="{{ old('house_number') }}" required autocomplete="house_number" autofocus>
+                                <input id="house_number" type="text" class="form-control @error('house_number') is-invalid @enderror" name="house_number" value="{{ old('house_number') }}">
 
                                 @error('house_number')
                                 <span class="invalid-feedback" role="alert">
@@ -71,7 +71,7 @@
                             <label for="city" class="col-md-4 col-form-label text-md-right">{{ __('Plaats') }}</label>
 
                             <div class="col-md-6">
-                                <input id="city" type="text" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}" required autocomplete="city" autofocus>
+                                <input id="city" type="text" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}">
 
                                 @error('city')
                                 <span class="invalid-feedback" role="alert">
@@ -85,7 +85,7 @@
                             <label for="zipcode" class="col-md-4 col-form-label text-md-right">{{ __('Postcode') }}</label>
 
                             <div class="col-md-6">
-                                <input id="zipcode" type="text" class="form-control @error('zipcode') is-invalid @enderror" name="zipcode" value="{{ old('zipcode') }}" required autocomplete="zipcode" autofocus>
+                                <input id="zipcode" type="text" class="form-control @error('zipcode') is-invalid @enderror" name="zipcode" value="{{ old('zipcode') }}">
 
                                 @error('zipcode')
                                 <span class="invalid-feedback" role="alert">
@@ -131,6 +131,18 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="no_robot" class="col-md-4 col-form-label text-md-right">{{ __('Ik ben geen robot:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="no_robot" type="checkbox" class="defaultCheckbox" name="no_robot" required autocomplete="no_robot">
+                                @error('no_robot')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
