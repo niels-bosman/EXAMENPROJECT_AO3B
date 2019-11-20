0db6020e836e0ccd25e7a38edc873ed893d3b362
Cleax18 <clemens.marwijk@student.graafschapcollege.nl>
2019-11-20 23:12:18 +0100

    HTML/CSS fixed

--- restaurant-de-graaf/resources/views/profiel/account_activated.blade.php
+++ restaurant-de-graaf/resources/views/profiel/account_activated.blade.php
@@ -10,6 +10,6 @@
     <div class="login container">
         <h1 class="login__heading">Uw account is geactiveerd!</h1>
         <h3 class="text-center">U kunt nu uw account gebruiken</h3>
-        <a class="content__button-link" style='' href="{{ url('/') }}">ga naar profiel</a>
+        <a class="go-back content__button--center button--primary" style='margin: 30px auto -30px auto' href="{{ url('/') }}">ga naar profiel</a>
     </div>
 @endsection
