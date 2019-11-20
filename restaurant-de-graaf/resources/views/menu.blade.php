634835773783a5b392d4f6037a3ab73f49aef346
Niels Bosman <nielsbosman33@icloud.com>
2019-11-20 11:38:24 +0100

    Menu basics

--- restaurant-de-graaf/resources/views/menu.blade.php
+++ restaurant-de-graaf/resources/views/menu.blade.php
@@ -6,7 +6,36 @@
 
 @endsection
 
+<?php
+use App\Subtype;
+use App\Product;
+?>
+
 @section('content')
-    <h1>Hier is onze menu</h1>
-    <p>Momenteel nog leeg :/</p>
+    <div class="menukaart container">
+        <h1 class="menukaart__heading">Ons menu</h1>
+        <div class="menukaart__menu">
+
+            @foreach($types as $type)
+                <h2 class="menukaart__type">{{$type->name}}</h2>
+
+                @foreach(App\Subtype::where('type_id', $type->id)->get() as $subtype)
+                    <h4 class="menukaart__subtype">{{$subtype->name}}</h4>
+
+                    <div class="menukaart__single-product">
+                        @foreach(App\Product::where('subtype', $subtype->id)->get() as $product)
+                            <article class="menukaart__product">
+                                <div class="menukaart__product-name">
+                                    {{$product->name}}
+                                </div>
+                                <div class="menukaart__product-price">
+                                    &euro; {{$product->price}}
+                                </div>
+                            </article>
+                        @endforeach
+                    </div>
+                @endforeach
+            @endforeach
+        </div>
+    </div>
 @endsection
