14339bce08b3c06640a72364919f0438dcf1818f
Niels Bosman <nielsbosman33@icloud.com>
2019-11-21 20:24:28 +0100

    Remove profile modal

--- restaurant-de-graaf/resources/js/app.js
+++ restaurant-de-graaf/resources/js/app.js
@@ -1,3 +1,13 @@
 $('.reservation__alert-close').on('click', function() {
     $('.reservation__alert').addClass('reservation__alert--dismissed');
 });
+
+$('.profiel__remove-account-button').on('click', function() {
+    $('.profiel__remove-modal').addClass('profiel__remove-modal--active');
+    $('.profiel__remove-modal-background').addClass('profiel__remove-modal-background--active');
+});
+
+$('.profiel__remove-modal-disable').on('click', function() {
+    $('.profiel__remove-modal').removeClass('profiel__remove-modal--active');
+    $('.profiel__remove-modal-background').removeClass('profiel__remove-modal-background--active');
+});
--- restaurant-de-graaf/resources/views/profiel/profiel.blade.php
+++ restaurant-de-graaf/resources/views/profiel/profiel.blade.php
@@ -83,10 +83,27 @@
                     <div class="col-md-6 profiel__spacing">
                         <button type="submit" class="button float-right button--primary">Wijzigen</button>
             </form>
-            <form method="post">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <button type="submit" class="button button--danger float-right">Account opzeggen</button>
+            <a class="button button--danger float-right profiel__remove-account-button" href="#">Account opzeggen</a>
+            <div class="profiel__remove-modal-background"></div>
+            <form method="post" class="profiel__remove-modal">
+                <div class="modal-header">
+                    <h5 class="modal-title">Weet je zeker dat je je account wilt opzeggen? Dit kan niet meer terug gezet worden.</h5>
+                    <button type="button" class="close profiel__remove-modal-disable">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="profiel__remove-modal-content">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label>Ik ben geen robot
+                                <input type="checkbox" required>
+                            </label>
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button type="submit" class="button button--danger float-right">Account opzeggen</button>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
