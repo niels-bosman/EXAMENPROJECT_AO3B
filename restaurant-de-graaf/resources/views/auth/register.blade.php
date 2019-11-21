d0ae56512a0928241d2495815184de801e5a8fa4
Niels Bosman <nielsbosman33@icloud.com>
2019-11-21 10:34:53 +0100

    Register form adjustments

--- restaurant-de-graaf/resources/views/auth/register.blade.php
+++ restaurant-de-graaf/resources/views/auth/register.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="login container">
+        <h1 class="login__heading">Registreren</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -18,7 +17,7 @@
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -26,7 +25,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="tel_number" class="col-md-4 col-form-label text-md-right">{{ __('Tel.') }}</label>
+                            <label for="tel_number" class="col-md-4 col-form-label text-md-right">{{ __('Telefoonnummer') }}</label>
 
                             <div class="col-md-6">
                                 <input id="tel_number" type="text" class="form-control @error('tel_number') is-invalid @enderror" name="tel_number" value="{{ old('tel_number') }}" required autocomplete="tel_number" autofocus>
@@ -96,13 +95,13 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Emailadres') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -116,7 +115,7 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -132,7 +131,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="no_robot" class="col-md-4 col-form-label text-md-right">{{ __('Ik ben geen robot:') }}</label>
+                            <label for="no_robot" class="col-md-4 col-form-label text-md-right">{{ __('Ik ben geen robot') }}</label>
 
                             <div class="col-md-6">
                                 <input id="no_robot" type="checkbox" class="defaultCheckbox" name="no_robot" required autocomplete="no_robot">
@@ -145,7 +144,7 @@
                         </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="button button--primary">
                                     {{ __('Registreren') }}
                                 </button>
                             </div>
@@ -155,5 +154,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
