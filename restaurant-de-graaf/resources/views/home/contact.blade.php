2aad7c757e73cab2b5b9cdc29064f4f46dfa812b
Cleax18 <clemens.marwijk@student.graafschapcollege.nl>
2019-11-22 14:52:12 +0100

    Modified Contact page

--- restaurant-de-graaf/resources/views/home/contact.blade.php
+++ restaurant-de-graaf/resources/views/home/contact.blade.php
@@ -26,7 +26,7 @@
                 </div>
                 <div class="form-group row">
                     <div class="col-md-12">
-                        <textarea id="form-content" rows="10" cols="30" class="form-control" required></textarea>
+                        <textarea id="form-content" rows="10" cols="30" class="form-control" required placeholder="Geachte heer/mevrouw van restaurant de graaf,"></textarea>
                     </div>
                 </div>
                 <div class="form-group row mb-0">
@@ -40,9 +40,11 @@
         </div>
         <div class="col-md-6">
             <div class="col-md-12">
-                <h2 class="text-center contact--h2">Telefonisch</h2>
+                <h2 class="text-center contact--h2">Contact persoon</h2>
                 <div class="contact--telephone">
-                    <h4 class="contact--h3">(06) 52 54 12 36 </h4>
+                    <h5 class="contact--h3">Lex Verhoeven</h3>
+                    <h6 class="contact--h3">Lex.Verhoeven@Gmail.com</h4>
+                    <h6 class="contact--h3">(06) 52 54 12 36 </h4>
                 </div>
             </div>
             <div class="contact--space">
@@ -58,8 +60,8 @@
         </div>
     </div>
 </div>
-<div>
-    <iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&height=600&hl=nl&q=J.F.%20kennedylaan%2049+(Restaurant%20de%20graaf)&ie=UTF8&t=&z=14&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+<div class="contact--iframe">
+    <iframe width="100%" height="300" src="https://maps.google.com/maps?width=100%&height=600&hl=nl&q=J.F.%20kennedylaan%2049+(Restaurant%20de%20graaf)&ie=UTF8&t=&z=14&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
         <a href="https://www.mapsdirections.info/nl/maak-een-google-map/">Maak een Google Map</a>
         van
         <a href="https://www.mapsdirections.info/nl/">
