736a230fc5e6661057bd1fc9eabee13ef0e85499
Cleax18 <clemens.marwijk@student.graafschapcollege.nl>
2019-11-25 15:06:47 +0100

    Fixed css and js for user pop up

--- restaurant-de-graaf/resources/views/beheer/user/users.blade.php
+++ restaurant-de-graaf/resources/views/beheer/user/users.blade.php
@@ -48,17 +48,17 @@
                     </div>
                 </form>
                 @if($user->blocked == 0)
-                <a class="button button--primary profiel__remove-account-button" data-id="{{$user->id}}" href="#"><i class="fas fa-ban"></i></a>
-                <div class="profiel__remove-modal-background profiel__remove-modal-disable" data-id="{{$user->id}}"></div>
-                <form method="post" class="profiel__remove-modal" data-id="{{$user->id}}">
+                <a class="button button--primary profiel__block-account-button" data-id="{{$user->id}}" href="#"><i class="fas fa-ban"></i></a>
+                <div class="profiel__block-modal-background profiel__block-modal-disable" data-id="{{$user->id}}"></div>
+                <form method="post" class="profiel__block-modal" data-id="{{$user->id}}">
                     <input type="hidden" name="id" value="{{$user->id}}">
                     <div class="modal-header">
                         <h5 class="modal-title">Weet je zeker dat je het account van {{$user->name}} wilt blokkeren?</h5>
-                        <button type="button" class="close profiel__remove-modal-disable">
+                        <button type="button" class="close profiel__block-modal-disable">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="profiel__remove-modal-content">
+                    <div class="profiel__block-modal-content">
                         <div class="row">
                             <div class="col-md-12">
                                 <label>Ik ben geen robot
