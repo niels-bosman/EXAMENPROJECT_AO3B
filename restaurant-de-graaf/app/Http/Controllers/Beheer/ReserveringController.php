a31d9117320fc1ff2176aed7b1edf096d786f80a
majd wenni <majd.wenni@student.graafschapcollege.nl>
2019-11-26 09:50:51 +0100

    Week ophalen reserveringen

---
+++ restaurant-de-graaf/app/Http/Controllers/Beheer/ReserveringController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Beheer;
+
+use App\Reservation;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Symfony\Component\Console\Input\Input;
+use DB;
+
+
+class ReserveringController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('beheer');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        if (isset($_GET['datum'])){
+            $reservations = Reservation::whereDate('date','=', $_GET['datum'])->get();
+        }
+        elseif(isset($_GET['week'])){
+            $date = Carbon::now();
+            $date->setISODate(2019, 48);
+            $weekEnd =  $date->startOfWeek();
+            $weekStart = $date->endOfWeek();
+            $weekStart->day -= 6;
+            $reservations = Reservation::whereDate('date', [$weekStart, $weekEnd])->get();
+//            $reservations = Reservation::where(\DB::raw("WEEKOFYEAR(date)"),'=',$_GET['week'])->get();
+
+//            dd(\request('week'));
+            dd($weekStart, $weekEnd);
+        }
+        else{
+            $reservations = Reservation::all();
+        }
+
+        return view('beheer/reserveringen',compact('reservations'));
+
+
+
+    }
+
+}
--- restaurant-de-graaf/routes/web.php
+++ restaurant-de-graaf/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/profiel/account_blocked_password', 'ProfileController@account_block
 
 Route::get('/beheer', 'Beheer\BeheerController@index');
 Route::get('/beheer/gebruikers', 'Beheer\BeheerController@index');
-Route::get('/beheer/reserveringen', 'Beheer\BeheerController@index');
+Route::get('/beheer/reserveringen', 'Beheer\ReserveringController@index');
 Route::get('/beheer/producten', 'Beheer\BeheerController@index');
 Route::get('/beheer/tafels', 'Beheer\BeheerController@index');
 
