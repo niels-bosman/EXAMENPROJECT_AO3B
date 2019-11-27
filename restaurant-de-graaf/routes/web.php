a880edf2e0498bf551d46ef9643d9ee03df3c4b3
Sietse <sietse.noordbruis@student.graafschapcollege.nl>
2019-11-27 14:15:37 +0100

    Reserveren als admin

--- restaurant-de-graaf/routes/web.php
+++ restaurant-de-graaf/routes/web.php
@@ -32,6 +32,8 @@ Route::get('/beheer/gebruikers/pdf/{reservation}', 'PDFController@index');
 Route::get('/beheer/gebruikers/{user}/block', 'Beheer\UserController@block');
 
 Route::get('/beheer/reserveringen', 'Beheer\ReserveringController@index');
+Route::get('/beheer/reserveren/new', 'Beheer\NieuweReserveringController@index'); //Route is afwijkend
+Route::post('/beheer/reserveren/new', 'Beheer\NieuweReserveringController@create'); //Route is afwijkend
 Route::get('/beheer/reserveringen/{id}', 'Beheer\BeheerController@get');
 Route::put('/beheer/reserveringen/{id}', 'Beheer\BeheerController@put');
 Route::delete('/beheer/reserveringen/{reservation_code}', 'Beheer\ReserveringController@delete');
