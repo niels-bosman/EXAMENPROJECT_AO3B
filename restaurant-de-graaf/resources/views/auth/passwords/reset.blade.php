792a4cb0db4638901e062260670830c230e68ac4
Niels Bosman <nielsbosman33@icloud.com>
2019-11-27 11:17:56 +0100

    Regex fix reset pass

--- restaurant-de-graaf/resources/views/auth/passwords/reset.blade.php
+++ restaurant-de-graaf/resources/views/auth/passwords/reset.blade.php
@@ -26,6 +26,11 @@
                     <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Bevestig Password') }}</label>
                     <div class="col-md-6">
                         <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                 </div>
                 <div class="form-group row mb-0">
