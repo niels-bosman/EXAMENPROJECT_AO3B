9ce8a3058a89d160e237a9c527ac13762ecb0e44
Niels Bosman <nielsbosman33@icloud.com>
2019-12-09 11:36:11 +0100

    Admin routes redirect to home

--- restaurant-de-graaf/routes/web.php
+++ restaurant-de-graaf/routes/web.php
@@ -20,38 +20,82 @@ Route::get('/profiel/account_not_activated', 'ProfileController@account_not_acti
 Route::get('/profiel/account_blocked', 'ProfileController@account_blocked');
 Route::get('/profiel/account_blocked_password', 'ProfileController@account_blocked_password');
 
-Route::get('/beheer', 'Beheer\BeheerController@index');
+//Route::get('/beheer', 'Beheer\BeheerController@index');
+Route::get('/beheer', 'ProfileController@home');
 
-Route::get('/beheer/klanten', 'Beheer\UserController@index');
-Route::put('/beheer/klanten', 'Beheer\UserController@block');
-Route::delete('/beheer/klanten', 'Beheer\UserController@destroy');
+//Route::get('/beheer/klanten', 'Beheer\UserController@index');
+Route::get('/beheer/klanten', 'ProfileController@home');
 
-Route::get('/beheer/klanten/{user}', 'Beheer\UserController@show');
-Route::put('/beheer/klanten/{user}', 'Beheer\UserController@update');
-Route::get('/beheer/klanten/pdf/{reservation}', 'PDFController@index');
-Route::get('/beheer/klanten/{user}/block', 'Beheer\UserController@block');
+//Route::put('/beheer/klanten', 'Beheer\UserController@block');
+Route::put('/beheer/klanten', 'ProfileController@home');
 
-Route::get('/beheer/reserveringen', 'Beheer\ReserveringController@index');
-Route::get('/beheer/reserveren/new', 'Beheer\NieuweReserveringController@index'); //Route is afwijkend
-Route::post('/beheer/reserveren/new', 'Beheer\NieuweReserveringController@create'); //Route is afwijkend
-Route::get('/beheer/reserveringen/{id}', 'Beheer\BeheerController@get');
-Route::put('/beheer/reserveringen/{id}', 'Beheer\BeheerController@put');
-Route::delete('/beheer/reserveringen/{reservation_code}', 'Beheer\ReserveringController@delete');
-Route::delete('/beheer/reserveringen/bestelling/{id}', 'Beheer\BeheerController@destroyBestelling');
+//Route::delete('/beheer/klanten', 'Beheer\UserController@destroy');
+Route::delete('/beheer/klanten', 'ProfileController@home');
 
-Route::get('/beheer/reserveringen/{id}/bestellingen/add', 'Beheer\BeheerController@getBestellingen');
-Route::post('/beheer/reserveringen/{id}/bestellingen/add', 'Beheer\BeheerController@postBestellingen');
+//Route::get('/beheer/klanten/{user}', 'Beheer\UserController@show');
+Route::get('/beheer/klanten/{user}', 'ProfileController@home');
 
-Route::get('/beheer/producten', 'Beheer\ProductController@index');
-Route::delete('/beheer/producten', 'Beheer\ProductController@delete');
-Route::post('/beheer/producten/new', 'Beheer\ProductController@post');
-Route::get('/beheer/producten/new', 'Beheer\ProductController@getNew');
-Route::get('/beheer/producten/{id}', 'Beheer\ProductController@get');
-Route::put('/beheer/producten/{id}', 'Beheer\ProductController@put');
+//Route::put('/beheer/klanten/{user}', 'Beheer\UserController@update');
+Route::put('/beheer/klanten/{user}', 'ProfileController@home');
 
-Route::get('/beheer/tafels', 'Beheer\TafelsController@index');
-Route::get('/beheer/tafels/{id}', 'Beheer\TafelsController@detail');
-Route::put('/beheer/tafels/{id}', 'Beheer\TafelsController@update');
+//Route::get('/beheer/klanten/pdf/{reservation}', 'PDFController@index');
+Route::get('/beheer/klanten/pdf/{reservation}', 'ProfileController@home');
+
+//Route::get('/beheer/klanten/{user}/block', 'Beheer\UserController@block');
+Route::get('/beheer/klanten/{user}/block', 'ProfileController@home');
+
+//Route::get('/beheer/reserveringen', 'Beheer\ReserveringController@index');
+Route::get('/beheer/reserveringen', 'ProfileController@home');
+
+//Route::get('/beheer/reserveren/new', 'Beheer\NieuweReserveringController@index');
+Route::get('/beheer/reserveren/new', 'ProfileController@home');
+
+//Route::post('/beheer/reserveren/new', 'Beheer\NieuweReserveringController@create');
+Route::post('/beheer/reserveren/new', 'ProfileController@home');
+
+//Route::get('/beheer/reserveringen/{id}', 'Beheer\BeheerController@get');
+Route::get('/beheer/reserveringen/{id}', 'ProfileController@home');
+
+//Route::put('/beheer/reserveringen/{id}', 'Beheer\BeheerController@put');
+Route::put('/beheer/reserveringen/{id}', 'ProfileController@home');
+//Route::delete('/beheer/reserveringen/{reservation_code}', 'Beheer\ReserveringController@delete');
+Route::delete('/beheer/reserveringen/{reservation_code}', 'ProfileController@home');
+
+//Route::delete('/beheer/reserveringen/bestelling/{id}', 'Beheer\BeheerController@destroyBestelling');
+Route::delete('/beheer/reserveringen/bestelling/{id}', 'ProfileController@home');
+
+//Route::get('/beheer/reserveringen/{id}/bestellingen/add', 'Beheer\BeheerController@getBestellingen');
+Route::get('/beheer/reserveringen/{id}/bestellingen/add', 'ProfileController@home');
+
+//Route::post('/beheer/reserveringen/{id}/bestellingen/add', 'Beheer\BeheerController@postBestellingen');
+Route::post('/beheer/reserveringen/{id}/bestellingen/add', 'ProfileController@home');
+
+//Route::get('/beheer/producten', 'Beheer\ProductController@index');
+Route::get('/beheer/producten', 'ProfileController@home');
+
+//Route::delete('/beheer/producten', 'Beheer\ProductController@delete');
+Route::delete('/beheer/producten', 'ProfileController@home');
+
+//Route::post('/beheer/producten/new', 'Beheer\ProductController@post');
+Route::post('/beheer/producten/new', 'ProfileController@home');
+
+//Route::get('/beheer/producten/new', 'Beheer\ProductController@getNew');
+Route::get('/beheer/producten/new', 'ProfileController@home');
+
+//Route::get('/beheer/producten/{id}', 'Beheer\ProductController@get');
+Route::get('/beheer/producten/{id}', 'ProfileController@home');
+
+//Route::put('/beheer/producten/{id}', 'Beheer\ProductController@put');
+Route::put('/beheer/producten/{id}', 'ProfileController@home');
+
+//Route::get('/beheer/tafels', 'Beheer\TafelsController@index');
+Route::get('/beheer/tafels', 'ProfileController@home');
+
+//Route::get('/beheer/tafels/{id}', 'Beheer\TafelsController@detail');
+Route::get('/beheer/tafels/{id}', 'ProfileController@home');
+
+//Route::put('/beheer/tafels/{id}', 'Beheer\TafelsController@update');
+Route::put('/beheer/tafels/{id}', 'ProfileController@home');
 
 
 Route::get('logout', 'Auth\LoginController@logout', function () {
